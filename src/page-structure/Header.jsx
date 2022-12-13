e79059e6d7ab2cd9044d2a020336d18d6f594a35
Renan Araújo <renan.araujoonline@gmail.com>
2022-12-13 14:46:38 -0300

    increased width in mobile header

--- src/page-structure/Header.jsx
+++ src/page-structure/Header.jsx
@@ -5,14 +5,14 @@ export default function Header(){
 
   return (
     <section className='wrapper'>
-      {/* mobile nav */}
+      {/* desktop nav */}
       <nav className='nav-desktop flex w-[50%] justify-around m-auto items-center'>
         <p className='text-[1.5rem] text-white font-bold underline'>About Us</p>
         <p className=' text-white font-bold tlogo text-[2rem]'>Green World</p>
         <p className='text-[1.5rem] text-white font-bold underline'>Contact Us</p>
       </nav>
-      {/* desktop nav */}
-      <nav className='nav-mobile flex w-[50%] justify-around m-auto items-center'>
+      {/* mobile nav */}
+      <nav className='nav-mobile flex w-[90%] justify-evenly m-auto items-center'>
         <p className='text-[1.1rem] text-white font-bold underline'>About</p>
         <p className='text-[1.4rem] text-white font-bold'>GW</p>
         <p className='text-[1.1rem] text-white font-bold underline'>Contact</p>
